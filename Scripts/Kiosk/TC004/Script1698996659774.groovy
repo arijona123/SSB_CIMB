b8c4e4ec7bff85687f531fb7ea8111dc3be58407
Arijona Purba <arijonapurba10@gmail.com>
2023-11-03 14:34:39 +0700

    coba edit

--- Scripts/Kiosk/TC004/Script1698996659774.groovy
+++ Scripts/Kiosk/TC004/Script1698996659774.groovy
@@ -17,3 +17,70 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser(url)
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Konvensional/img_Digital Lounge  Branch_img-fluid'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Konvensional/input__dlCode'), 'DAE')
+
+WebUI.click(findTestObject('Konvensional/button selanjutnya DAE/button_Selanjutnya'))
+
+WebUI.delay(5)
+
+WebUI.click(findTestObject('cob/scroll/Page_CIMB Niaga/video_cimb'))
+
+WebUI.click(findTestObject('Konvensional/button_BUKA REKENING BARU'))
+
+WebUI.delay(5)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.click(findTestObject('Konvensional/button_Konvensional'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.delay(5)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/button_TABUNGAN BERJANGKA'))
+
+WebUI.delay(3)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/button_Saving Plan'))
+
+WebUI.delay(3)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/dropdown_saya telah membaca'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/span_Saya telah membaca, memahami, dan menyetujui Syarat dan Ketentuan yang berlaku dan fitur-fitur produk'))
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/button_Lanjut_saya telah membaca'))
+
+WebUI.delay(3)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/button_Oke_keyboard'))
+
+WebUI.delay(3)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.selectOptionByIndex(findTestObject('Konvensional/Tabungan berjangka/div_rekening sumber dana'), '703195358100 - XTRA SAVERS IDR - RP 100.000.000.000', 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.acceptAlert()
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/div_frekuensi menabung'))
+
+WebUI.click(findTestObject('Konvensional/Tabungan berjangka/li_BULANAN'))
+
+WebUI.closeBrowser()
+
